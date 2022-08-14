096256bc430adfd6b235a51e7448cdf140532d60
mingyuchoo <mingyuchoo@gmail.com>
2022-08-15 08:10:56 +0900

    Update install-runtimes-for-ubunut.sh

--- Ubuntu/install-runtimes-for-ubunut.sh
+++ Ubuntu/install-runtimes-for-ubunut.sh
@@ -142,7 +142,7 @@ rm elm
 # Install Java
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 jabba install openjdk@1.11.0
-jabba default openjdk@1.11.0
+jabba alias default openjdk@1.11.0
 java --version
 
 # Install Doom Emacs
