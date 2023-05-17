aa2b03e7a92963ffcdfe98e6009bc0716f3fba93
mingyuchoo <mingyuchoo@gmail.com>
2023-05-18 06:48:34 +0900

    added

---
+++ Ubuntu/bin/conn-mysql.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Please install mysql client before to use
+#   sudo apt install -y mysql-client
+
+mysql --host=localhost --port=3306 --user=root --password=root --database=test
---
+++ Ubuntu/bin/conn-psql.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Please install below before to use
+#   sudo apt install -y postgresql-client-common
+#   sudo apt install -y postgresql-client
+
+psql -h localhost -p 5432 -d postgres -U postgres -W
