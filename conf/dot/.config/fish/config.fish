7c75e8a756c328e4487589251705ac279eabac00
mingyuchoo <mingyuchoo@gmail.com>
2024-11-09 11:23:44 +0900

    updated

--- conf/dot/.config/fish/config.fish
+++ conf/dot/.config/fish/config.fish
@@ -1,30 +1,33 @@
 stty erase ^H
 
+
 umask 022
 
+
 # ENVs
-set -gx EDITOR 'emacsclient -t'
-set -gx VISUAL 'emacsclient -t'
+set -gx EDITOR hx
+set -gx VISUAL hx
 set -gx LANG en_US.UTF-8
 set -gx MANPATH "/usr/local/man:$MANPATH"
-set -gx PAGER 'cat'
+set -gx PAGER bat
 
-set -x HIST_STAMPS "yyyy-mm-dd"
+
+set -x HIST_STAMPS yyyy-mm-dd
 set -x fish_history_size 20000
-set -x JAVA_HOME /usr/libexec/java_home
+#set -x JAVA_HOME /usr/libexec/java_home
+
 
 # PATHs
-fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/sbin
-fish_add_path /usr/local/bin
-fish_add_path /Applications/Docker.app/Contents/Resources/bin
-fish_add_path /Applications/Visual Studio Code.app/Contents/Resources/app/bin
-fish_add_path $HOME/Applications/Zed.app/Contents/MacOS
-fish_add_path $HOME/Applications/Alacritty.app/Contents/MacOS
+#fish_add_path /opt/homebrew/bin
+#fish_add_path /opt/homebrew/sbin
+#fish_add_path /usr/local/bin
 fish_add_path $HOME/bin
+fish_add_path $HOME/go/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.config/emacs/bin
+fish_add_path $HOME/.emacs.d/bin
+fish_add_path $HOME/.ghcup/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.opam/bin
 fish_add_path $HOME/.rd/bin
@@ -32,12 +35,16 @@ fish_add_path $HOME/.rd/bin
 
 # ALIASes
 alias emacs="emacsclient -nw -c -a 'emacs'"
-alias ls="ls --color=auto"
-alias ll="ls -l"
-alias la="ls -la"
+alias ls="eza"
+alias ll="eza -l"
+alias la="eza -la"
+alias lt="eza --tree"
+alias ldk="lazydocker"
 alias mkdir="mkdir -p"
-alias vi="nvim"
-alias vim="nvim"
+alias sbcl='rlwrap sbcl'
+alias tree="eza --tree"
+alias vi="hx"
+alias vim="hx"
 
 
 # FUNCTIONs
@@ -51,11 +58,10 @@ function y
     rm -f -- $tmp
 end
 
-# Sources
-source "$HOME/.venv/bin/activate.fish"
 
 # EVALs
-eval "$(zoxide   init fish)"
+eval "$(direnv   hook fish)"
 eval "$(starship init fish)"
+eval "$(zoxide   init fish)"
 eval "$(opam env --switch=default)"
-eval "$(fnm env --use-on-cd)"
+eval "$(fnm env --use-on-cd --shell fish)"
