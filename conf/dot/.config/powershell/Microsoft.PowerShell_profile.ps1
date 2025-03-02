0f0c2ee223aa8a02c78e8a0148d5b4171ed1f795
my-name <my-email@address.com>
2025-03-02 17:01:33 +0900

    added powershell

---
+++ conf/dot/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,50 @@
+# make error messages readable
+$host.privatedata.ErrorBackgroundColor = 'DarkRed'
+$host.privatedata.ErrorForegroundColor = 'White'
+
+# make strings readable
+$colors = @{}
+$colors['String'] = [System.ConsoleColor]::Cyan
+$colors['Comment'] = [System.ConsoleColor]::Gray
+
+# emacs key
+Set-PSReadLineOption -EditMode Emacs -Colors $colors
+
+### for Starship
+Invoke-Expression (&starship init powershell)
+
+## for Yazi
+function y {
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+    Set-Location -LiteralPath $cwd
+  }
+  Remove-Item -Path $tmp
+}
+
+## for alias for PowerShell
+Set-Alias -Name ll -Value Get-ChildItem -Option AllScope -Scope Global -Force
+
+# #############################################################################
+# Azure CLI
+# #############################################################################
+Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+  param($commandName, $wordToComplete, $cursorPosition)
+  $completion_file = New-TemporaryFile
+  $env:ARGCOMPLETE_USE_TEMPFILES = 1
+  $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+  $env:COMP_LINE = $wordToComplete
+  $env:COMP_POINT = $cursorPosition
+  $env:_ARGCOMPLETE = 1
+  $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+  $env:_ARGCOMPLETE_IFS = "`n"
+  $env:_ARGCOMPLETE_SHELL = 'powershell'
+  az 2>&1 | Out-Null
+  Get-Content $completion_file | Sort-Object | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+  }
+  Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+}
+
